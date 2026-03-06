264ca5d5f388219d59a863a6c2bd9ca318db6f40
lucasvidalouca0098-art <lucasvidalouca0098@gmail.com>
2026-03-06 05:35:34 -0300

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -81,7 +81,7 @@ RECOVERY_SDCARD_ON_DATA := true
 TARGET_USES_MKE2FS := true
 
 # TWRP specific build flags
-TW_DEVICE_VERSION := -Android-Artisan_v1-
+TW_DEVICE_VERSION := -Alessandro-
 TW_THEME := portrait_hdpi
 TW_BRIGHTNESS_PATH := "/sys/class/backlight/panel/brightness"
 TW_CUSTOM_CPU_TEMP_PATH := "/sys/class/thermal/thermal_zone0/temp"
